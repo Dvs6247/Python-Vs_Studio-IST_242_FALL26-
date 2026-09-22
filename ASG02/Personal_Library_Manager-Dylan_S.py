ee1a31a32d612f2a084933097eb51791dff2e0fa
Dvs6247 <dvs6247@psu.edu>
2026-09-22 01:04:29 -0400

    Added the for function within the search_list function for how the list is shown.

--- ASG02/Personal_Library_Manager-Dylan_S.py
+++ ASG02/Personal_Library_Manager-Dylan_S.py
@@ -59,7 +59,11 @@ def search_list(book_list, title = None, author = None, year = None):
      a = author.lower().strip() if author else None
      y = year.lower().strip() if year else None
      #Used to have a clean output of the title, author, and year.
-     
+     for book in book_list:
+          book_title = str(book.get('title', ''))
+          book_author = str(book.get('author', ''))
+          book_year = str(book.get('year', ''))
+          #Added the for function to how it lists the book's year, author, and title when requested.
 
 def main():
     display_menu()
