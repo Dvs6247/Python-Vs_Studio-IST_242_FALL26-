a3367a93134bef593c8c1eb0ada62f394922be91
Dvs6247 <dvs6247@psu.edu>
2026-09-24 14:16:26 -0400

    Added for manufacturer.py: class manufacturer +init function w/ self strings name + country (Also accidentally committed certain changes in the ASG02 file)

---
+++ ASG03(vehicles_D.S.)/main.py
@@ -0,0 +1,5 @@
+"""The main entry point for ASGO3 
+is Vehicle Hierarchy with ABC"""
+
+if __name__ == "__main__":
+    main()
---
+++ ASG03(vehicles_D.S.)/manufacturer.py
@@ -0,0 +1,10 @@
+class manufacturer:
+    def __init__(self, name: str, country: str):
+        self._name = name
+        self._country = country
+        """Added a class manufacturer to 
+        use the function __init__ 
+        for helping the class function
+        and the function _init__ has two strings with self
+        called name and country"""
+        
